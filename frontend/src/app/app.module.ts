5b48996d9bb28303d53c237c6f1a8e989d089482
Choi Dahye <choi.dahye424@gmail.com>
2022-09-05 16:51:27 +0900

    generate components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitleBarComponent } from './layout/title-bar/title-bar.component';
+import { BottomBarComponent } from './layout/bottom-bar/bottom-bar.component';
+import { LayoutMainComponent } from './layout/layout-main/layout-main.component';
+import { SideBarComponent } from './layout/layout-main/side-bar/side-bar.component';
+import { ExplorerComponent } from './layout/layout-main/explorer/explorer.component';
+import { LayoutContentComponent } from './layout/layout-main/layout-content/layout-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleBarComponent,
+    BottomBarComponent,
+    LayoutMainComponent,
+    SideBarComponent,
+    ExplorerComponent,
+    LayoutContentComponent
   ],
   imports: [
     BrowserModule,
